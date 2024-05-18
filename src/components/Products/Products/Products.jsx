0d92fdf4f7ae69315f0e170a793a5f7bbb9a0e97
webNekit <mochalov-hunter@mail.ru>
2024-05-19 00:40:33 +0300

    Обновил адаптив

--- src/components/Products/Products/Products.jsx
+++ src/components/Products/Products/Products.jsx
@@ -8,25 +8,25 @@ const Products = ({ title = null }) => {
         <Container>
             <h2 className="text-3xl text-gray-950 font-semibold">{title}</h2>
             <div className="w-full flex gap-2 flex-col md:flex-row md:gap-5 pt-4">
-            <div className="flex items-center gap-2">
+            <div className="flex items-start md:items-center gap-2 flex-col md:flex-row">
                     <span className="text-gray-700 font-medium">Категория</span>
-                    <select className="bg-gray-950 text-white p-3">
+                    <select className="bg-gray-950 text-white p-3 w-full">
                         <option>Кроссовки</option>
                         <option>Кеды</option>
                         <option>Ботинки</option>
                     </select>
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex items-start md:items-center gap-2 flex-col md:flex-row">
                     <span className="text-gray-700 font-medium">Сортировать</span>
-                    <select className="bg-gray-950 text-white p-3">
+                    <select className="bg-gray-950 text-white p-3 w-full">
                         <option>По возрастанию цены</option>
                         <option>По убыванию цены</option>
                         <option>Новинки</option>
                     </select>
                 </div>
-                <div className="flex items-center gap-2">
+                <div className="flex items-start md:items-center gap-2 flex-col md:flex-row">
                     <span className="text-gray-700 font-medium">Бренд</span>
-                    <select className="bg-gray-950 text-white p-3">
+                    <select className="bg-gray-950 text-white p-3 w-full">
                         <option>Nike</option>
                         <option>Adidas</option>
                         <option>Новинки</option>
