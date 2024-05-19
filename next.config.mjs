c9202fda2ffefa6967b7859ebdca327d0c3ed63a
webNekit <mochalov-hunter@mail.ru>
2024-05-20 01:10:28 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+    reactStrictMode: true,
     images: {
         domains: ['shoe-ecommerce-backend-wm0e.onrender.com', 'localhost']
     }
