ceb693b2ceecdbc074dbb5f51d986c62dee5c909
webNekit <mochalov-hunter@mail.ru>
2024-05-20 01:33:46 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: [process.env.NEXT_PUBLIC_STRAPI_API_URL, 'localhost', 'https://shoe-ecommerce-backend-wm0e.onrender.com', 'shoe-ecommerce-backend-wm0e.onrender.com']
+        domains: ['localhost']
     }
 };
 
