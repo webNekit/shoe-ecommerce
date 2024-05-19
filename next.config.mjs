21325946925581e88fe71ad778dce206038c974f
webNekit <mochalov-hunter@mail.ru>
2024-05-19 16:41:41 +0300

    create hook

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ["localhost"]
+        domains: ["localhost", "https://shoe-ecommerce-backend-wm0e.onrender.com"]
     }
 };
 
