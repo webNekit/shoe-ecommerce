18bb523ff0d55e7fb3116927a50f7b2a2c9eabb4
webNekit <mochalov-hunter@mail.ru>
2024-05-27 10:18:53 +0300

    Поменяли изображение

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        // domains: ['localhost'],
         remotePatterns: [
             {
-                protocol: "http",
-                hostname: "localhost",
-                port: "1337",
-                pathname: "/uploads/**/*",
-            },
-            {
                 protocol: "https",
-                hostname: "shoe-ecommerce-backend-wm0e.onrender.com"
+                hostname: "**"
             }
         ],
     },
